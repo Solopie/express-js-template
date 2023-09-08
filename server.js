816b225ad714f7079bb9eb062a4b3b79e0025850
Jesse Nguyen <jesse_nguyen9@yahoo.com>
2023-09-08 20:25:48 +1000

    Logging

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const config = require("./utils/config");
+const morgan = require("morgan");
 const express = require("express");
+
 const app = express();
+
 app.set("view engine", "pug");
-
-
+app.use(morgan("combined"));
 app.use((req, res, next) => {
     res.setHeader("X-Robots-Tag", "none");
     next();
 });
-
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
